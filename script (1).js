b7c24871c73753df266e340a84451249605243b6
akhilakky <109684460+akhilakky@users.noreply.github.com>
2022-07-20 21:45:48 +0530

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -168,7 +168,7 @@ function checkImageWithNyckel(image)
     formdata.append('file', image);
 
     $.ajax({
-        url:"https://www.nyckel.com/v1/functions/ivj8zz62hjq3yw9l/invoke",
+        url:"https://www.nyckel.com/v1/functions/redgjpefebyogf2p/invoke",
         type: 'post',
         data: formdata,
         contentType: false,
